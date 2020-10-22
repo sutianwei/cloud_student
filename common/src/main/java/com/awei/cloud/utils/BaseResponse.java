bb43b08107eb37fdfbf1fe22bb56fa7db3a2dc2b
sutianwei <18700871054@163.com>
2020-10-22 18:18:30 +0800

    封装全局返回格式

---
+++ common/src/main/java/com/awei/cloud/utils/BaseResponse.java
@@ -0,0 +1,106 @@
+package com.awei.cloud.utils;
+
+
+
+public class BaseResponse<T> {
+
+    private boolean  success;
+
+    /**
+     * 状态码
+     */
+    private String code;
+
+    private String msg;
+
+    private T data;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public static <T> BaseResponse<T>  success(String msg){
+        BaseResponse<T> response = new BaseResponse<T>();
+        response.setSuccess(true);
+        response.setMsg(msg);
+        response.setData(null);
+        return response;
+    }
+    public static <T> BaseResponse<T> success(T obj, String msg) {
+        BaseResponse<T> response = new BaseResponse<T>();
+        response.setSuccess(true);
+        response.setMsg(msg);
+        response.setData(obj);
+        return response;
+    }
+
+    /**
+     * 处理成功返回
+     *
+     * @return
+     */
+    public static <T> BaseResponse<T> error(T obj, String msg) {
+        BaseResponse<T> response = new BaseResponse<T>();
+        response.setSuccess(false);
+        response.setMsg(msg);
+        response.setData(obj);
+        return response;
+    }
+
+    /**
+     * 处理异常返回
+     *
+     * @param msg
+     * @return
+     */
+    public static <T> BaseResponse<T> error(String msg) {
+        BaseResponse<T> response = new BaseResponse<T>();
+        response.setSuccess(false);
+        response.setMsg(msg);
+        response.setData(null);
+        return response;
+    }
+
+    /**
+     * 处理警告返回
+     *
+     * @param msg
+     * @return
+     */
+    public static <T> BaseResponse<T> warn(String msg) {
+        BaseResponse<T> response = new BaseResponse<T>();
+        response.setSuccess(false);
+        response.setCode("warn");
+        response.setMsg(msg);
+        response.setData(null);
+        return response;
+    }
+}
