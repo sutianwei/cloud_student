62032258232671a79680c39342ec48f5ebb7cbd5
sutianwei <18700871054@163.com>
2020-12-08 23:11:46 +0800

    redis 实现排行榜

---
+++ student_role/src/main/java/com/awei/cloud/controller/rankController.java
@@ -0,0 +1,58 @@
+package com.awei.cloud.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.awei.cloud.utils.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.DefaultTypedTuple;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@RestController
+public class rankController {
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    public static final String SCORE_RANK = "score_rank";
+
+    @PostMapping("rank")
+    public void batchAdd() {
+        Set<ZSetOperations.TypedTuple<String>> tuples = new HashSet<>();
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            DefaultTypedTuple<String> tuple = new DefaultTypedTuple<>("张三" + i, 1D + i);
+            tuples.add(tuple);
+        }
+        System.out.println("循环时间:" +( System.currentTimeMillis() - start));
+        Long num = redisTemplate.opsForZSet().add(SCORE_RANK, tuples);
+        System.out.println("批量新增时间:" +(System.currentTimeMillis() - start));
+        System.out.println("受影响行数：" + num);
+    }
+
+    @PostMapping("/top_ten")
+    public void list() {
+
+        Set<String> range = redisTemplate.opsForZSet().reverseRange(SCORE_RANK, 0, 10);
+        System.out.println("获取到的排行列表:" + JSON.toJSONString(range));
+        Set<ZSetOperations.TypedTuple<String>> rangeWithScores = redisTemplate.opsForZSet().reverseRangeWithScores(SCORE_RANK, 0, 10);
+        System.out.println("获取到的排行和分数列表:" + JSON.toJSONString(rangeWithScores));
+    }
+
+    @PostMapping("/add")
+    public void add() {
+        redisTemplate.opsForZSet().add(SCORE_RANK, "李四", 8899);
+    }
+    @PostMapping("get")
+    public void find(){
+        Long rankNum = redisTemplate.opsForZSet().reverseRank(SCORE_RANK, "李四");
+        System.out.println("李四的个人排名：" + rankNum);
+
+        Double score = redisTemplate.opsForZSet().score(SCORE_RANK, "李四");
+        System.out.println("李四的分数:" + score);
+    }
+}
