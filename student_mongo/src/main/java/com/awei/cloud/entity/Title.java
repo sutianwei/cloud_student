1dd93782dc730c2acd43f07f2fe855a86c21ebcb
sutianwei <18700871054@163.com>
2021-01-10 22:10:49 +0800

    添加测试

--- student_mongo/src/main/java/com/awei/cloud/entity/Title.java
+++ student_mongo/src/main/java/com/awei/cloud/entity/Title.java
@@ -2,6 +2,7 @@ package com.awei.cloud.entity;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.io.Serializable;
 
@@ -9,16 +10,19 @@ import java.io.Serializable;
 public class Title implements Serializable {
 
     private static final long serialVersionUID = 729918189840157911L;
+
     @Id
     private String id;
 
     /**
      * 题目
      */
+    @Field("topic")
     private String topic;
     /**
      * 答案
      */
+    @Field("item")
     private String item;
 
     public String getId() {
